7079aaa4144fe498a4a9f310cf6146af3cfd0f24
ismahaneyy <liciousmayrah@gmail.com>
2023-01-12 12:33:44 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+
+
+
+function EyesOnMe(){
+    return (
+        <button onBlur={e =>{
+            console.log("Hey! Eyes on me!")
+        }} onFocus={e =>{
+            console.log("Good!")
+        }}>Eyes on me</button>
+    )
+}
+
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
+
 function Keypad (){
     return (
-        <div></div>
+        
+        <div>
+            <input type='Password' onChange={e =>{
+                console.log("Entering password...")
+            }}></input>
+        </div>
     )
 }
 
